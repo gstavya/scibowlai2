58373f2d2b4459aafa563bac40ae670a6fd52bc7
gstavya <137587109+gstavya@users.noreply.github.com>
2025-03-23 12:25:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -266,4 +266,5 @@ def update_points():
         return jsonify({"error": "User not found"}), 404
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
